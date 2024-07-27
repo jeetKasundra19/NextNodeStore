33177e3897af9ebe7286709810fb2684115e229e
jeetKasundra19 <kasundra529jeet@gmail.com>
2024-07-28 00:22:05 +0530

    setUp shadcn in vue js and setup project

--- classic90-panel/vite.config.js
+++ classic90-panel/vite.config.js
@@ -4,12 +4,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
