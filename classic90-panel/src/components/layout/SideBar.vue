eac54e6910da670dd51ecfbcb8423f09844a8cc2
jeetKasundra19 <kasundra529jeet@gmail.com>
2024-08-12 22:44:00 +0530

    Update SideBar.vue

--- classic90-panel/src/components/layout/SideBar.vue
+++ classic90-panel/src/components/layout/SideBar.vue
@@ -58,7 +58,7 @@ const sidebarLinkList = ref([
         title: 'Income',
         childLinks: [
           {
-            id: '#sublink2',
+            id: '#sublink20',
             title: 'Earnings',
             goToPath: '/',
             activePaths: ['/']
